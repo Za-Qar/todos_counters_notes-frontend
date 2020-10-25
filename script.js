54c89782fc433343f1b4b83904bd8c7050db57fe
= <za.qa@outlook.com>
2020-10-25 08:29:42 +0000

    Safe keeping

--- script.js
+++ script.js
@@ -1,145 +1,162 @@
-// DOM Elements
-const categoryElement = document.querySelector(".category");
-const currentQuestion = document.querySelector(".currentQuestion");
-const difficultyElement = document.querySelector(".difficulty");
+//DOM elements
+const input = document.querySelector("#input");
+const addButton = document.querySelector("#addButton");
 const ul = document.querySelector("ul");
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const btn3 = document.querySelector("#btn3");
-const btn4 = document.querySelector("#btn4");
-const mainElement = document.querySelector("#main");
-const startGame = document.querySelector("#start");
-const endScore = document.querySelector("#yourScore");
-
-let scoreElement = 0;
-let round = 1;
-
-function toggle() {
-  startGame.classList.toggle("hide");
-  mainElement.classList.toggle("hide");
-  trivia();
-}
+//Input buttons
+const inp2 = document.querySelector(".column2");
+const inp3 = document.querySelector(".column3");
+const inp4 = document.querySelector(".column4");
+const inp5 = document.querySelector(".column5");
 
 
-startGame.addEventListener("click", toggle);
-
-// Fetch
-async function trivia() {
-  const response = await fetch(
-    "https://opentdb.com/api.php?amount=1&type=multiple"
-  );
-  const data = await response.json();
-  const results = data.results[0];
-  const {
-    category,
-    correct_answer,
-    incorrect_answers,
-    difficulty,
-    question,
-  } = results;
-
-  console.log(category);
-  console.log(correct_answer);
-  console.log(incorrect_answers[0]);
-  console.log(difficulty);
-  console.log(question);
-
-  triviaText(category, correct_answer, incorrect_answers, difficulty, question);
-  for (let i = ul.children.length; i >= 0; i--) {
-    ul.appendChild(ul.children[Math.random() * i | 0]);
+//add todo list
+function add() {
+  if (input.value === ""){
+    alert("You must add a todo")
+    return;
   }
-}
 
-// Embed DOM Functions
-function triviaText(
-  category,
-  correct_answer,
-  incorrect_answers,
-  difficulty,
-  question
-) {
-  categoryElement.innerText = `Category: ${category}`;
-  currentQuestion.innerHTML = question;
-  btn1.innerHTML = correct_answer;
-  btn2.innerHTML = incorrect_answers[0];
-  btn3.innerHTML = incorrect_answers[1];
-  btn4.innerHTML = incorrect_answers[2];
-  difficultyElement.innerText = `Difficulty: ${difficulty}`;
-}
-
-// Score
-function incrementScore() {
-  scoreElement = scoreElement + 10;
-  document.querySelector(".score").innerHTML = `Score: ${scoreElement}`;
-  setTimeout(trivia, 1000);
-  correctAnswer();
-  setTimeout(btnWhite1, 1000);
-  endGame();
-  incrementRound();
-}
-
-function decrementScore() {
-  scoreElement = scoreElement - 5;
-  document.querySelector(".score").innerHTML = `Score: ${scoreElement}`;
-  setTimeout(trivia, 1000);
-  correctAnswer();
-  setTimeout(btnWhite1, 1000);
-  endGame();
-  incrementRound();
-}
-
-function incrementRound() {
-  round++;
-  document.querySelector(".round").innerHTML = `Round: ${round}/5`;
-}
-
-function correctAnswer() {
-  btn1.style.backgroundColor = "green";
-}
-
-function btnWhite1() {
-  btn1.style.backgroundColor = "white";
-}
-
-function incorrectAnswer() {
-  btn2.style.backgroundColor = "red";
-}
-
-function btnWhite2() {
-  btn2.style.backgroundColor = "white";
-}
-
-
-
+  //add li
+  let newLi = document.createElement("li");
+  newLi.innerText = input.value;
+  if (inp2.checked){
+    newLi.classList.add("yellowgreen");
+  } else if (inp3.checked){
+    newLi.classList.add("red")
+  } else if (inp4.checked){
+    newLi.classList.add("pink")
+  } else if (inp5.checked){
+    newLi.classList.add("burlywood")
+  } else {
+    newLi.classList.add("listStyle");
+  }
   
-btn1.addEventListener("click", incrementScore);
-btn2.addEventListener("click", decrementScore);
-btn3.addEventListener("click", decrementScore);
-btn4.addEventListener("click", decrementScore);
-/*
-- add eventlister to buttons
-- the function of the event listers will be:
-    - if current answer increment score 
-    - if asnwer is incorrect, decrement score
-    - if answer is corrent, button turns green
-    - if answer is incorrect, button turns red
-- Return after 5 rounds and display score
-- When game starts, input username
-- Store score in username variable and display after 5 rounds
-- When 5 rounds are over, allow user to input a new username
-- Show all user scores at the end
-- 
-*/
+  ul.appendChild(newLi);
 
-// End of game
-// Store score variable
-// Reset score and round variables
+  //add x button
+  let span = document.createElement("span");
+  span.innerText = "\u00D7";
+  span.classList.add("xButton");
+  newLi.appendChild(span);
 
-function endGame() {
-  if (round === 5) {
-    mainElement.classList.toggle("hide");
-    let yourScore = document.createElement("h3");
-    yourScore.innerText = `Your score is ${scoreElement}`;
-    endScore.appendChild(yourScore);
+  input.value = "";
+
+  newLi.addEventListener("click", throughText);
+
+  //Delete list when xButton is clicked
+    function removeList(){
+    newLi.classList.add("hide");
+  };
+
+  span.addEventListener("click", removeList);
+};
+
+addButton.addEventListener("click", add);
+input.addEventListener("keypress", function(e){
+  if (e.key === 'Enter'){
+    add()
   }
+})
+
+//Mark as completed
+function throughText(newLi) {
+  newLi.target.classList.toggle("through");
+};
+
+// Weather API
+// const axios = require('axios').default;
+// const params = {
+//   access_key: '11493fd1d1949604507bed259f91ba61',
+//   query: 'New York'
+// }
+
+// axios.get('https://api.weatherstack.com/current', {params})
+//   .then(response => {
+//     const apiResponse = response.data;
+//     console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`);
+//   }).catch(error => {
+//     console.log(error);
+//   });
+
+// async function weather(){
+//   let response = await fetch("http://api.weatherstack.com/");
+//   console.log(response)
+// }
+
+// weather();
+
+const apiKey = "caf81d1304dba9d89805ecde571c22c4";
+
+// GET WEATHER FROM API PROVIDER
+async function getWeather(latitude, longitude){
+  let api = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`);
+  console.log(api)
+  let data = await api.json();
+  let results = data.main
+  // console.log(results)
+  let currentSituation = data.weather[0].description;
+  let {
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    pressure,
+  } = results;
+  
+  temp = Math.floor(results.temp-273.15);
+  feels_like = Math.floor(results.feels_like-273.15);
+  temp_min = Math.floor(results.temp_min-273.15);
+  temp_max = Math.floor(results.temp_max-273.15);
+
+  const currentTemp = document.querySelector("#currentTemp");
+  const situation = document.querySelector("#situation");
+  const maxTemp = document.querySelector("#maxTemp");
+  const minTemp = document.querySelector("#minTemp");
+  const feel = document.querySelector("#feel");
+
+  currentTemp.innerText = temp + "°C";
+  situation.innerText = currentSituation;
+  maxTemp.innerText = "Maximum: " + temp_max + "°C";
+  minTemp.innerText = "Minimum: " + temp_min + "°C";
+  feel.innerText = "Feels like: " + feels_like + "°C";
 }
+
+getWeather(52.4862, -1.8904)
+
+
+//-----------------Time and Date------------------//
+function dateTime(){
+  let currentdate = new Date(); 
+
+  //adding zero to hours, minutes and seconds
+  let hr = currentdate.getHours()
+  if (hr < 10){
+    hr = "0" + hr
+  }
+
+  let min = currentdate.getMinutes()
+  if (min < 10){
+    min = "0" + min
+  }
+
+  let sec = currentdate.getSeconds()
+  if (sec < 10){
+    sec = "0" + sec
+  }
+
+  let dateValue = currentdate.getDate() + "/"
+                  + (currentdate.getMonth()+1)  + "/" 
+                  + currentdate.getFullYear();
+  let timeValue = hr + ":"  
+                  + min + ":" 
+                  + sec;
+
+  const time = document.querySelector("#time");
+  const date = document.querySelector("#date");
+
+  time.innerText = timeValue
+  date.innerText = dateValue
+}
+
+setInterval(dateTime, 1000)
+//-----------------End - Time and Date - END------------------//
