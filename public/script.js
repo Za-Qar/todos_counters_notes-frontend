4d66e11bb05006695efba6879c88bfa3449656de
= <za.qa@outlook.com>
2020-11-23 00:19:15 +0000

    backup

--- public/script.js
+++ public/script.js
@@ -349,12 +349,36 @@ function addCounter() {
 
 addCounterBtn.addEventListener("click", addCounter);
 
-//
-//
-//
-//
-//Are these the functions that I must call ??
+// Make movable items
+const ulTodos = document.querySelector("#ul");
+new Sortable(ulTodos, {
+  handle: ".listStyle",
+  animation: 200,
+});
 
-// let createCounter = fetch(`http://localhost:5000/createCounter`).then((res) =>
-//   res.json()
-// );
+new Sortable(counterUl, {
+  handle: "span",
+  animation: 200,
+});
+
+//Make image enlarge
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
