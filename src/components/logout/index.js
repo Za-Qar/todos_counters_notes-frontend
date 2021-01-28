922d6bcf5bf4bf69508bda619ad4c62afc71d6a1
= <za.qa@outlook.com>
2021-01-28 18:57:28 +0000

    logout button styled

--- src/components/logout/index.js
+++ src/components/logout/index.js
@@ -22,7 +22,7 @@ function Logout() {
               onClick={() => {
                 firebase.auth().signOut();
               }}
-              className="logoutButton"
+              className="loginButton"
             >
               Logout
             </button>
